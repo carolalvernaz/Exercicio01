bb2f4f82ebfd3f979f30f9138c0356e4b39b711d
carol <carol@DESKTOP-BNIJN3Q>
2023-03-02 21:45:58 -0300

    Commit para o Exercicio01

---
+++ Unidade01/src/PackageDeTeste/Somar2.java
@@ -0,0 +1,21 @@
+package PackageDeTeste;
+import java.util.*;
+
+public class Somar2 {
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main (String args[]){
+		//Declaracao de variaveis
+		int num1, num2, soma;
+		//Leituras
+		System.out.println("Digite um número");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro número");
+		num2 = sc.nextInt();
+		//Somar
+		soma = num1 + num2;
+		//Mostrar na tela
+		System.out.println("Soma:" + soma);
+		}
+
+}
